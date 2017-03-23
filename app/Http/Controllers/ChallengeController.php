88bf490efab62c0224fdd89725a2818315f65904
joechoy123321 <joechoy1305@gmail.com>
2017-03-23 08:52:34 +0800

    re upload the GQQP

--- app/Http/Controllers/ChallengeController.php
+++ app/Http/Controllers/ChallengeController.php
@@ -61,7 +61,7 @@ class ChallengeController extends Controller
                 Session::push('challenge', $totalquestiondetail);
                 $playNumber++;
                 if($this->checkEnd($playNumber)) {
-                    dd(Session::get('challenge'));
+                    return view ($this->goPage());
                 }
                 return view('challengemode', compact('mc', 'totalgold', 'totalknowledge', 'playNumber'));
             }
@@ -94,7 +94,7 @@ class ChallengeController extends Controller
             Session::push('challenge',$totalquestiondetail);
             $playNumber++;
             if($this->checkEnd($playNumber)) {
-                $this->goPage();
+                return view ($this->goPage());
             }
             return view('challengemode', compact('mc', 'totalgold', 'totalknowledge', 'playNumber'));
         }
